64f41cab1f69bc5bf36fa67ca0659c3c4582596f
gcornejov <g.cornejovivado@gmail.com>
2024-06-10 21:13:00 -0400

    Squashed commit of the following:
    commit cc7a1f02c6319fa0a845fd46240acccba9bdbad2
    Author: gcornejov <g.cornejovivado@gmail.com>
    Date:   Mon Jun 10 00:39:01 2024 -0400
    
        Add unit tests
    
    commit ae748526872eca5cc34337732f4f3bdb52c6c449
    Author: gcornejov <g.cornejovivado@gmail.com>
    Date:   Mon May 27 00:23:08 2024 -0400
    
        Add method to manage the game session
    
    commit 2a7555ba3a05d0238f2e49d8bb80955098321ea8
    Author: gcornejov <g.cornejovivado@gmail.com>
    Date:   Mon May 27 00:04:52 2024 -0400
    
        Improve class abrstractions
    
    commit 3c7756c30e373933d75b3253ab1073388d20ebfd
    Author: gcornejov <g.cornejovivado@gmail.com>
    Date:   Thu May 23 00:52:32 2024 -0400
    
        Add interactivity with win condition
    
    commit 15439a2ca30e95f052456175227455b62026c4e2
    Author: gcornejov <g.cornejovivado@gmail.com>
    Date:   Tue May 21 22:32:20 2024 -0400
    
        Add logic to store puzzle solution
    
    commit f091097017591cdd112926ec3094785b58e3c58c
    Author: gcornejov <g.cornejovivado@gmail.com>
    Date:   Tue May 21 01:21:31 2024 -0400
    
        Add logic to generate initial board
    
    commit 118762cd18ee25a802509e8a9dcee77e3a6126a0
    Author: gcornejov <g.cornejovivado@gmail.com>
    Date:   Mon May 20 19:31:58 2024 -0400
    
        Setup environment

---
+++ pycross/game.py
@@ -0,0 +1,115 @@
+import os
+
+class Grid:
+    def __init__(
+        self,
+        rows_guides: list[int],
+        columns_guides: list[int],
+        solution_matrix: list[list[int]] = None,
+    ):
+        self._max_row_len: int = len(max(rows_guides, key=len))
+        self._max_col_len: int = len(max(columns_guides, key=len))
+
+        self._rows_guides: list[int] = rows_guides
+        self._columns_guides: list[int] = columns_guides
+
+        total_rows: int = len(rows_guides)
+        total_cols: int = len(columns_guides)
+        self._board_matrix: list[list[int]] = [
+            [0] * total_cols for _ in range(total_rows)
+        ]
+
+        self._solution_matrix: list[list[int]] = self._check_solution_matrix(
+            solution_matrix, total_rows, total_cols
+        )
+
+        self._top_board, self._left_board = self._draw_board_borders()
+        self._screen_board: str = self._draw_board()
+
+    def _check_solution_matrix(
+        self, solution_matrix: list[list[int]], total_rows: int, total_columns: int
+    ) -> list[list[int]]:
+        if len(solution_matrix) != total_rows:
+            raise ValueError("Unmatching number of rows")
+
+        for row in solution_matrix:
+            if len(row) != total_columns:
+                raise ValueError("Unmatching number of columns")
+
+        return solution_matrix
+
+    def _draw_board_borders(self) -> tuple[str, list[int]]:
+        top_offset: str = "   " * self._max_row_len
+        top_board: str = top_offset
+        for i in range(self._max_col_len - 1, -1, -1):
+            for j in range(len(self._columns_guides)):
+                if i < len(self._columns_guides[j]):
+                    top_board += f" {str(self._columns_guides[j][i])} "
+                else:
+                    top_board += "   "
+            if i > 0:
+                top_board += f"\n{top_offset}"
+
+        left_board: list = []
+        for i in range(len(self._rows_guides)):
+            guides_row: str = ""
+            for j in range(self._max_row_len - 1, -1, -1):
+                if j < len(self._rows_guides[i]):
+                    guides_row += f" {str(self._rows_guides[i][j])} "
+                else:
+                    guides_row += "   "
+
+            left_board.append(guides_row)
+
+        return top_board, left_board
+
+    def _draw_board(self):
+        bottom_board: str = ""
+        for i, guides_row in enumerate(self._left_board):
+            cells = "][".join("\u25a0" if _ else " " for _ in self._board_matrix[i])
+            bottom_board += f"{guides_row}[{cells}]\n"
+
+        return f"{self._top_board}\n{bottom_board}"
+
+    def check_solved(self) -> bool:
+        for i in range(len(self._board_matrix)):
+            for j in range(len(self._board_matrix[i])):
+                if self._board_matrix[i][j] != self._solution_matrix[i][j]:
+                    return False
+
+        return True
+
+    def draw_pixel(self, input: str) -> None:
+        delete_pixel: bool = True if input.startswith("d/") else False
+        coordinates: str = input.removeprefix("d/").split(",")
+
+        if len(coordinates) != 2:
+            print("Enter coordinates in the correct format (x,y)")
+            return
+
+        try:
+            x, y = int(coordinates[0]) - 1, int(coordinates[1]) - 1
+            self._board_matrix[x][y] = 0 if delete_pixel else 1
+            self._screen_board = self._draw_board()
+        except ValueError:
+            print("Enter numeric (int) values for the coordinates")
+        except IndexError:
+            print("Enter coordinates within the grid borders")
+
+    def start_game(self) -> None: # pragma: no cover
+        win = None
+        while not win:
+            print(self)
+            coordinates = input(
+                "Enter coordinates to paint <x,y> (Prefix 'd/' to delete pixel): "
+            )
+            os.system("cls" if os.name == "nt" else "clear")
+            self.draw_pixel(coordinates)
+            win = self.check_solved()
+
+        os.system("cls" if os.name == "nt" else "clear")
+        print("You won!!!")
+        print(self)
+
+    def __repr__(self) -> str:
+        return self._screen_board
---
+++ tests/test_game.py
@@ -0,0 +1,221 @@
+import pytest
+
+from pycross.game import Grid
+
+
+@pytest.fixture
+def test_grid_data() -> tuple:
+    return (
+        # Rows Guides
+        [[3], [1], [1, 1], [1, 1, 1], [1, 1]],
+        # Columns Guides
+        [[1], [1, 1, 1], [1, 2], [1, 1, 1], [1]],
+        # Solution matrix
+        [
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [1, 0, 1, 0, 1],
+            [0, 1, 0, 1, 0],
+        ],
+    )
+
+
+@pytest.fixture
+def test_grid_obj(test_grid_data: tuple) -> Grid:
+    return Grid(*test_grid_data)
+
+
+def test_grid(test_grid_obj: Grid):
+    rows_guides: list[int] = [[3], [1], [1, 1], [1, 1, 1], [1, 1]]
+    cols_guides: list[int] = [[1], [1, 1, 1], [1, 2], [1, 1, 1], [1]]
+    board_matrix: list[int] = [[0] * 5 for _ in range(5)]
+
+    assert (test_grid_obj._max_row_len == 3) and (test_grid_obj._max_col_len == 3)
+    assert (test_grid_obj._rows_guides == rows_guides) and (test_grid_obj._columns_guides == cols_guides)
+    assert test_grid_obj._board_matrix == board_matrix
+
+
+def test_check_solution_matrix(test_grid_obj: Grid):
+    solution: list[int] = [
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 1],
+        [0, 1, 0, 1, 0],
+    ]
+
+    checked_solution: list[int] = test_grid_obj._check_solution_matrix(solution, 5, 5)
+
+    assert checked_solution == solution
+
+@pytest.mark.parametrize(
+    "rows",
+    [
+        ([0, 0, 1, 0, 0]),
+        ([0, 1, 1, 1], [0, 0, 1, 0, 0])
+    ]
+)
+def test_check_solution_matrix_exception(test_grid_obj: Grid, rows: list[int]):
+    solution: list[int] = [
+        *rows,
+        [0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 1],
+        [0, 1, 0, 1, 0],
+    ]
+
+    with pytest.raises(ValueError):
+        test_grid_obj._check_solution_matrix(solution, 5, 5)
+
+
+def test_draw_board_borders(test_grid_obj: Grid):
+    grid_top: str = (
+        "             1     1    \n"
+        "             1  2  1    \n"
+        "          1  1  1  1  1 "
+    )
+    grid_left: str = [
+        "       3 ", 
+        "       1 ", 
+        "    1  1 ", 
+        " 1  1  1 ", 
+        "    1  1 "
+    ]
+    
+    border_top, border_left = test_grid_obj._draw_board_borders()
+
+    assert grid_top == border_top and grid_left == border_left
+
+def test_draw_board(test_grid_obj: Grid):
+    board_draw = (
+        "             1     1    \n"
+        "             1  2  1    \n"
+        "          1  1  1  1  1 \n"
+        "       3 [ ][ ][ ][ ][ ]\n"
+        "       1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+        " 1  1  1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+    )
+    
+    screen_board: str = test_grid_obj._draw_board()
+
+    assert board_draw == screen_board
+
+@pytest.mark.parametrize(
+    "p, result",
+    [
+        (0, True),
+        (1, False),
+    ]
+)
+def test_check_solved(test_grid_obj: Grid, p: int, result: bool):
+    test_grid_obj._board_matrix = [
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, p, 1, 0],
+        [1, 0, 1, 0, 1],
+        [0, 1, 0, 1, 0],
+    ]
+
+    solved = test_grid_obj.check_solved()
+
+    assert solved == result
+
+def test_draw_pixel(test_grid_obj: Grid):
+    input: str = "3,3"
+    board_draw: str = (
+        "             1     1    \n"
+        "             1  2  1    \n"
+        "          1  1  1  1  1 \n"
+        "       3 [ ][ ][ ][ ][ ]\n"
+        "       1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][\u25a0][ ][ ]\n"
+        " 1  1  1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+    )
+
+    test_grid_obj.draw_pixel(input)
+
+    assert test_grid_obj._board_matrix[2][2] == 1
+    assert board_draw == test_grid_obj._screen_board
+
+def test_clear_pixel(test_grid_obj: Grid):
+    input: str = "d/3,3"
+    board_draw: str = (
+        "             1     1    \n"
+        "             1  2  1    \n"
+        "          1  1  1  1  1 \n"
+        "       3 [ ][ ][ ][ ][ ]\n"
+        "       1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+        " 1  1  1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+    )
+    test_grid_obj._board_matrix[2][2] = 1
+
+    test_grid_obj.draw_pixel(input)
+
+    assert test_grid_obj._board_matrix[2][2] == 0
+    assert board_draw == test_grid_obj._screen_board
+
+def test_draw_pixel_wrong_format(test_grid_obj: Grid):
+    input: str = "3.3"
+    board_draw: str = (
+        "             1     1    \n"
+        "             1  2  1    \n"
+        "          1  1  1  1  1 \n"
+        "       3 [ ][ ][ ][ ][ ]\n"
+        "       1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+        " 1  1  1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+    )
+
+    test_grid_obj.draw_pixel(input)
+
+    assert test_grid_obj._board_matrix[2][2] == 0
+    assert board_draw == test_grid_obj._screen_board
+
+@pytest.mark.parametrize(
+    "input, exception_class, out_msg",
+    [
+        ("x,y", ValueError, "Enter numeric (int) values for the coordinates\n"),
+        ("6,2", IndexError, "Enter coordinates within the grid borders\n"),
+    ]
+)
+def test_draw_pixel_exception(capfd, test_grid_obj: Grid, input: str, exception_class: Exception, out_msg: str):
+    board_draw: str = (
+        "             1     1    \n"
+        "             1  2  1    \n"
+        "          1  1  1  1  1 \n"
+        "       3 [ ][ ][ ][ ][ ]\n"
+        "       1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+        " 1  1  1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+    )
+
+    test_grid_obj.draw_pixel(input)
+    exc_msg: str = capfd.readouterr()[0]
+
+    assert exc_msg == out_msg
+    assert test_grid_obj._board_matrix[2][2] == 0
+    assert board_draw == test_grid_obj._screen_board
+
+def test_start_game():
+    pass
+
+def test_repr(test_grid_obj: Grid):
+    board_draw: str = (
+        "             1     1    \n"
+        "             1  2  1    \n"
+        "          1  1  1  1  1 \n"
+        "       3 [ ][ ][ ][ ][ ]\n"
+        "       1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+        " 1  1  1 [ ][ ][ ][ ][ ]\n"
+        "    1  1 [ ][ ][ ][ ][ ]\n"
+    )
+
+    assert board_draw == str(test_grid_obj)
